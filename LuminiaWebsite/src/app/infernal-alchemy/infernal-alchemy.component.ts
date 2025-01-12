7aec12f217338381e959ec8268e453350e7adc3e
Wocowoco <33019109+Wocowoco@users.noreply.github.com>
2025-01-13 00:03:31 +0100

    Add activities to Infernal Alchemy

--- LuminiaWebsite/src/app/infernal-alchemy/infernal-alchemy.component.ts
+++ LuminiaWebsite/src/app/infernal-alchemy/infernal-alchemy.component.ts
@@ -11,6 +11,25 @@ export class InfernalAlchemyComponent implements OnInit {
   researchPointsTotal : number = 7;
   amountOfMoney : number = 402;
 
+  earningsData = [
+    {range: "< 9", result: "-50 gold"},
+    {range: "10-14", result: "25 gold"},
+    {range: "15-19", result: "75 gold"},
+    {range: "20-24", result: "100 gold"},
+    {range: "25-29", result: "125 gold"},
+    {range: "30-34", result: "150 gold"},
+    {range: "35-39", result: "175 gold"},
+    {range: "40-44", result: "200 gold"},
+    {range: "> 45", result: "250 gold"}
+  ];
+
+  researchData = [
+    {price: "25 gold", dc: "20"},
+    {price: "50 gold", dc: "15"},
+    {price: "75 gold", dc: "11"},
+    {price: "100 gold", dc: "8"},
+  ];
+  displayedColumns: string[] = ["dc", "effect"]
   constructor() { }
 
   ngOnInit(): void {
--- LuminiaWebsite/src/app/infernal-alchemy/infernal-alchemy.module.ts
+++ LuminiaWebsite/src/app/infernal-alchemy/infernal-alchemy.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { InfernalAlchemyComponent } from './infernal-alchemy.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 
 const childRoutes: Routes = [
   {path:"infernal-alchemy", component: InfernalAlchemyComponent }
@@ -15,7 +17,9 @@ const childRoutes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(childRoutes),
-    MatGridListModule
+    MatGridListModule,
+    MatTableModule,
+    MatCardModule
   ]
 })
 export class InfernalAlchemyModule { }
