c9f382454478c2c59400c1510856bfcbf054acb0
Wocowoco <33019109+Wocowoco@users.noreply.github.com>
2025-01-29 15:50:39 +0100

    Added a calendar for each quarter, with navigation in between them. Hardcoded events on calendar for now

--- LuminiaWebsite/src/app/calendar/calendar.component.ts
+++ LuminiaWebsite/src/app/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
+  currentQuarter : number = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clickedBloomen() : void {
+    this.currentQuarter = 1;
+  }
+
+  clickedSumsun() : void {
+    this.currentQuarter = 2;
+  }
+
+  clickedLeaflet() : void {
+    this.currentQuarter = 3;
+  }
+
+  clickedFrizwa() : void {
+    this.currentQuarter = 4;
   }
 
 }
--- LuminiaWebsite/src/app/calendar/calendar.module.ts
+++ LuminiaWebsite/src/app/calendar/calendar.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const childRoutes: Routes = [
   {path:"calendar", component: CalendarComponent }
@@ -16,7 +19,10 @@ const childRoutes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forRoot(childRoutes),
-    MatGridListModule
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTooltipModule
   ]
 })
 export class CalendarModule { }
