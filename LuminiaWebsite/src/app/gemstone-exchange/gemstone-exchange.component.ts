c58895219a4063f585b8abde81c744e59b9cb9b2
Wocowoco <33019109+Wocowoco@users.noreply.github.com>
2025-06-04 21:32:43 +0200

    Add hardcoded gemstone graph

--- LuminiaWebsite/src/app/gemstone-exchange/gemstone-exchange.component.ts
+++ LuminiaWebsite/src/app/gemstone-exchange/gemstone-exchange.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { LegendPosition, ScaleType } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-gemstone-exchange',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GemstoneExchangeComponent implements OnInit {
 
+  legendPosition : LegendPosition = LegendPosition.Below;
+  view: [number, number] = [window.innerWidth * 0.95, window.innerHeight *0.85];
+
+  lineChartData = [
+  {
+    name: 'Ruby',
+    series: [
+      { name: 1, value: 10 },
+      { name: 2, value: 15 },
+      { name: 3, value: 25 },
+    ]
+  },
+  {
+    name: 'Sapphire',
+    series: [
+      { name: 1, value: 5 },
+      { name: 2, value: 12 },
+      { name: 3, value: 8 },
+    ]
+  },
+  {
+    name: 'Emerald',
+    series: [
+      { name: 1,  value: 4 },
+      { name: 2,  value: 7 },
+      { name: 3,  value: 13 },
+    ]
+  }
+  ];
+
+  colorScheme = {
+    name: 'customScheme',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: ['#e53935', '#1e88e5', '#608c26']
+  };
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @HostListener('window:resize')
+  onResize() {
+    this.view = [window.innerWidth * 0.95, window.innerHeight * 0.85];
+  }
+
+  dateFormatter(dayNumber: number): string {
+    var months = ['Bloomen', 'Sumsun', 'Leaflet', 'Frizwa'];
+    const year = Math.floor(dayNumber / 364);
+    const monthIndex = Math.floor((dayNumber % 364) / 91);
+    const dayInMonth = (dayNumber % 91);
+    const monthName = months[monthIndex] || '???';
+    return dayInMonth + " " + monthName + " " + year;
+  }
 }
--- LuminiaWebsite/src/app/gemstone-exchange/gemstone-exchange.module.ts
+++ LuminiaWebsite/src/app/gemstone-exchange/gemstone-exchange.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { GemstoneExchangeComponent } from './gemstone-exchange.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 const childRoutes: Routes = [
   {path:"gemstone-exchange", component: GemstoneExchangeComponent }
 ]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GemstoneExchangeComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(childRoutes),
+    NgxChartsModule
   ]
 })
 export class GemstoneExchangeModule { }
